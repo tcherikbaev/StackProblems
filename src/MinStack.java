a848f20b775c32f4db8c9ee3baa9c4d72456ce71
Tilek <tcherikbaev@na.edu>
2024-06-06 18:40:25 -0500

    main method

--- src/MinStack.java
+++ src/MinStack.java
@@ -19,7 +19,7 @@ public class MinStack {
         return head.min;
     }
 
-    private class Node{
+    private static class Node{
         int val;
         int min;
 
@@ -32,13 +32,28 @@ public class MinStack {
             this.next=next;
         }
     }
+    public static void main(String[] args) {
+        MinStack obj = new MinStack();
+
+        // Test push method
+        obj.push(5);
+        obj.push(3);
+        obj.push(7);
+        obj.push(1);
+
+        System.out.println("Current min: " + obj.getMin()); // Output: 1
+
+        obj.pop();
+
+        System.out.println("Current min after one pop: " + obj.getMin()); // Output: 3
+
+        // Test top method
+        System.out.println("Current top: " + obj.top()); // Output: 7
+        obj.pop();
+        System.out.println("Current min after another pop: " + obj.getMin()); // Output: 3
+        obj.pop();
+        System.out.println("Current min after another pop: " + obj.getMin()); // Output: 5
+        System.out.println("Current top after all pops: " + obj.top()); // Output: 5
+    }
 }
 
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
